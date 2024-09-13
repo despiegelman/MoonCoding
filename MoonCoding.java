48a3cd6fe4a1d44a9bbb89663026548e56d29910
despiegelman <157067356+despiegelman@users.noreply.github.com>
2024-09-13 14:55:24 -0400

    Create MoonCoding.java

---
+++ MoonCoding.java
@@ -0,0 +1,8 @@
+public class MoonCoding{
+    public static void main (String[]args){
+        int moonDistance = 380000;
+        int speed = 10;
+        double hoursToMoon = (double) moonDistance/speed;
+        System.out.println(hoursToMoon);
+    }
+}
